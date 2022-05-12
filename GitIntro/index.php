091f3dce939d7bf43ded2fca1ddda53642f7ed9e
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-12 01:03:59 -0400

    Add eric's info

--- GitIntro/index.php
+++ GitIntro/index.php
@@ -7,7 +7,13 @@
     </head>
     <body>
         <?php
-        echo "Hello World!";
+        echo "<h2>Hello World!</h2>";
+        echo "</br>Eric Charnesky, Doctor of Engineering in Automotive Systems and Mobility, "
+        . "I'm taking the class because I'm the prof";
+        echo "</br>I've been programming for 15 years";
+        echo "</br>I like playing Dunegons and Dragons";
+        echo "</br>We're hoping to adopt baby #7 in July";
+        echo "</br>I'm training for my 9th marathon, I run the Freep International in October";
         ?>
     </body>
 </html>
