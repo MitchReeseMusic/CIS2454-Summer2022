6d28753d169643fbed4f82d4f73b0c9b96ee8c7e
MitchReeseMusic <MitchReeseMusic@gmail.com>
2022-05-15 15:24:51 -0400

    Hello World/Git Intro

---
+++ GitIntro/nbproject/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2>Hello World!</h2>";
+        echo
+            "</br>1 - Name, and major / reason for taking the class
+            </br>Mitch Reese - Part of my degree program, in school for software engineering!
+            </br>2 - Any previous programming experience that they have
+            </br>I have been programming for a little over 2 years.
+            </br>3 - Do they have any hobbies?
+            </br>I'm a big gamer, I spend way too much time playing and thinking about D&D, and I've been playing guitar for most of my life.
+            </br>4 - Do they have any plans for the summer?
+            </br>Just school and work really - I'm a full time Technical Recruiter by day, so that takes up a lot of my time. In my spare time i'm working on an original campaign setting book for D&D5E!
+            </br>5 - Anything else they'd like the class to know about them.
+            </br>Nothing in particular !"
+        ?>
+    </body>
+</html>
