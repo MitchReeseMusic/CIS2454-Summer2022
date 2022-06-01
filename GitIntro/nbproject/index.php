9c5331191fef4578908ccce2ee340c470b91a1d5
MitchReeseMusic <MitchReeseMusic@gmail.com>
2022-06-01 18:01:24 -0400

    Update index.php

--- GitIntro/nbproject/index.php
+++ GitIntro/nbproject/index.php
@@ -9,15 +9,20 @@
         echo "<h2>Hello World!</h2>";
         echo
             "</br>1 - Name, and major / reason for taking the class
-            </br>Mitch Reese - Part of my degree program, in school for software engineering!
+
+            </br>Ali Fayed - Part of my Software Engineering associated degree program, then transferring to get a BS degree in Computer Science.
+
             </br>2 - Any previous programming experience that they have
-            </br>I have been programming for a little over 2 years.
+
+            </br>I have been programming for more than 4 years and I have developed many applications (Web, Mobile, and Desktop).
+
             </br>3 - Do they have any hobbies?
-            </br>I'm a big gamer, I spend way too much time playing and thinking about D&D, and I've been playing guitar for most of my life.
-            </br>4 - Do they have any plans for the summer?
-            </br>Just school and work really - I'm a full time Technical Recruiter by day, so that takes up a lot of my time. In my spare time i'm working on an original campaign setting book for D&D5E!
-            </br>5 - Anything else they'd like the class to know about them.
-            </br>Nothing in particular !"
+
+            </br>I love traveling, camping, soccer, and my favorite is working out, which made me interested in nutrition.
+
+           </br> 4 - Do they have any plans for the summer?
+
+            </br>I am planning to go to Florida this summer with my friends."
         ?>
     </body>
 </html>
